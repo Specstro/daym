d1b0ea814044762a904886292209cc0a2eb8bbaf
Ryan <muhammadmidun19@gmail.com>
2023-01-16 23:14:31 +0800

    Update replit.nix

--- replit.nix
+++ replit.nix
@@ -1,8 +1,18 @@
 { pkgs }: {
-	deps = [
-		pkgs.nodejs-16_x
-        pkgs.nodePackages.typescript-language-server
-        pkgs.yarn
-        pkgs.docker
-	];
+    deps = [
+
+        pkgs.nodejs-16_x
+        pkgs.libwebp
+        pkgs.python
+	      pkgs.nodePackages.typescript
+        pkgs.libuuid
+        pkgs.ffmpeg
+        pkgs.imagemagick  
+        pkgs.wget
+        pkgs.git
+        pkgs.nodePackages.pm2
+    ];
+  env ={
+    LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [pkgs.libuuid];
+  };
 }
